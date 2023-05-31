74e408813046cf6a1aff41c451d97eb6d957726b
Bappy71 <19103076@iubat.edu>
2023-05-31 17:01:31 +0600

    commit message

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="Hero-left">Left Side</div>
+      <div className="Hero-right">Right Side</div>
+    </div>
+  );
+};
+
+export default Hero;
