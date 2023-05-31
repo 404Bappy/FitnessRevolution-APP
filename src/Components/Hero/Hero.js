78a2cda8233e447739b01bc7e6908b6950d19956
Bappy71 <19103076@iubat.edu>
2023-05-31 17:11:25 +0600

    commit message

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "../Header/Header";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="Hero-left">Left Side</div>
+      <div className="Hero-left">
+        <Header />
+      </div>
       <div className="Hero-right">Right Side</div>
     </div>
   );
