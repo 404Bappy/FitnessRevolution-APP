01191b6abbc5a960363ab939e35ec0bb08e9aba7
Bappy71 <19103076@iubat.edu>
2023-06-02 10:36:42 +0600

    commit message

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,10 +7,18 @@ const Hero = () => {
     <div className="hero">
       <div className="Hero-left">
         <Header />
-
+        {/* THE BEST ADD SECTIONS */}
         <div className="the-best-add">
           <div></div>
           <span>The Best Fitness Club In The Town</span>
+        </div>
+
+        {/* HERO HEADING SECTION*/}
+        <div className="hero-text">
+          <div>
+            <span>SHAPE</span>
+            <span> YOUR</span>
+          </div>
         </div>
       </div>
       <div className="Hero-right">Right Side</div>
