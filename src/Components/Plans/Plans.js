e0cb466fa3118a66fa9d72bfbca9a49f373c9e57
Bappy71 <19103076@iubat.edu>
2023-06-05 04:15:42 +0600

    first commit

---
+++ src/Components/Plans/Plans.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Plans.css";
+
+const Plans = () => {
+  return (
+    <div className="plans-container">
+      <div className="programs-header" style={{ gap: "2rem " }}>
+        <span className="strokeText">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="strokeText">NOW WITHUS</span>
+      </div>
+    </div>
+  );
+}; 
+
+export default Plans;
