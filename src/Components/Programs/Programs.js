c118a3b8c3e49ca716a3732a76f25f991bf3ffde
Bappy71 <19103076@iubat.edu>
2023-06-05 00:48:51 +0600

    first commit

--- src/Components/Programs/Programs.js
+++ src/Components/Programs/Programs.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./Programs.css";
 
 const Programs = () => {
-  return <div className="programs" id="programs">
-      
-  </div>;
+  return (
+    <div className="programs" id="programs">
+      {/* Header Section  */}
+      <div className="programs-header">
+        <span>Explore Our</span>
+        <span>Programs</span>
+        <span>to Shape You </span>
+      </div>
+    </div>
+  );
 };
 
 export default Programs;
