24cf051c190dd1184ae07e7904a4e299809ea532
Bappy71 <19103076@iubat.edu>
2023-06-05 04:29:59 +0600

    first commit

--- src/Components/Plans/Plans.js
+++ src/Components/Plans/Plans.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { plansData } from "../../data/plansData";
+import WhiteTick from "../../assets/whiteTick.png";
 import "./Plans.css";
 
 const Plans = () => {
@@ -9,8 +11,27 @@ const Plans = () => {
         <span>YOUR JOURNEY</span>
         <span className="strokeText">NOW WITHUS</span>
       </div>
+      {/* Plans Card */}
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>$ {plan.price}</span>
+
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature">
+                  <img src={WhiteTick} alt="" />
+                  <span key={i}>{feature}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}; 
+};
 
 export default Plans;
