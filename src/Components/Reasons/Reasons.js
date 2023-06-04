0c06e20bee2aa8bbfe4f7495bd1dfeb3c44f0378
Bappy71 <19103076@iubat.edu>
2023-06-05 03:55:47 +0600

    first commit

--- src/Components/Reasons/Reasons.js
+++ src/Components/Reasons/Reasons.js
@@ -3,6 +3,11 @@ import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
+import tick from "../../assets/tick.png";
+import nb from "../../assets/nb.png";
+import nike from "../../assets/nike.png";
+import adidas from "../../assets/adidas.png";
+
 import "./Reasons.css";
 
 const Reasons = () => {
@@ -15,7 +20,40 @@ const Reasons = () => {
         <img src={image4} alt="" />
       </div>
 
-      <div className="Reason-Right">Right Side of The Reason PART</div>
+      <div className="Reason-Right">
+        <span>Some Reasons</span>
+        <div>
+          <span className="strokeText">Why </span>
+          <span>Choose Us?</span>
+        </div>
+
+        <div className="Right-Detail">
+          <div>
+            <img src={tick} alt="" />
+            <span>OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+        <span style={{ color: "var(--gray)", fontWeight: "normal" }}>
+          OUR PARTNERS
+        </span>
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
