0a267bde6a5abd7197a4ba884700b6735ccccf82
Bappy71 <19103076@iubat.edu>
2023-06-05 20:26:04 +0600

    first commit

---
+++ src/Components/Testimonials/Testimonials.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Testimonials.css";
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials">
+      <div className="Left-Testi"></div>
+      <div className="Right-Testi"></div>
+    </div>
+  );
+};
+
+export default Testimonials;
