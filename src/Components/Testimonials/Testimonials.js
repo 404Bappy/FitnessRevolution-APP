f4327428bb8d66cbfe8100dbcf4898438d726768
Bappy71 <19103076@iubat.edu>
2023-06-06 00:29:14 +0600

    first commit

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Testimonials.css";
+import { testimonialsData } from "../../data/testimonialsData";
 
 const Testimonials = () => {
+  const [selected, setSelected] = useState(0);
+  const testiLength = testimonialsData.length;
   return (
     <div className="testimonials">
       <div className="Left-Testi">
         <span>Testimonials</span>
         <span className="strokeText">what they</span>
         <span>say about us</span>
+        <span>{testimonialsData[selected].review}</span>
+        <span>
+          <span style={{ color: "var(--orange)" }}>
+            {testimonialsData[selected].name}
+          </span>{" "}
+          - {testimonialsData[selected].status}
+        </span>
       </div>
       <div className="Right-Testi"></div>
     </div>
