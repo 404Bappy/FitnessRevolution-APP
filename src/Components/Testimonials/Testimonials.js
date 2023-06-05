a95f894edc2d4bd1284f7e00c48c8f95e00fea27
Bappy71 <19103076@iubat.edu>
2023-06-06 01:27:00 +0600

    first commit

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -22,11 +22,30 @@ const Testimonials = () => {
         </span>
       </div>
       <div className="Right-Testi">
+        <div></div>
+        <div></div>
         <img src={testimonialsData[selected].image} alt="" />
 
         <div className="arrows">
-          <img src={leftArrow} alt="" />
-          <img src={rightArrow} alt="" />
+          <img
+            onClick={() => {
+              selected === 0
+                ? setSelected(testiLength - 1)
+                : setSelected((prev) => prev - 1);
+            }}
+            src={leftArrow}
+            alt=""
+          />
+
+          <img
+            onClick={() => {
+              selected === testiLength - 1
+                ? setSelected(0)
+                : setSelected((prev) => prev + 1);
+            }}
+            src={rightArrow}
+            alt=""
+          />
         </div>
       </div>
     </div>
