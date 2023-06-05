a0b8cbf172051937addf2dec079358fa698986e8
Bappy71 <19103076@iubat.edu>
2023-06-05 20:34:36 +0600

    first commit

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -4,7 +4,11 @@ import "./Testimonials.css";
 const Testimonials = () => {
   return (
     <div className="testimonials">
-      <div className="Left-Testi"></div>
+      <div className="Left-Testi">
+        <span>Testimonials</span>
+        <span className="strokeText">what they</span>
+        <span>say about us</span>
+      </div>
       <div className="Right-Testi"></div>
     </div>
   );
