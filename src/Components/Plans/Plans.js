59d21a67e57bd49c2f8111358794e6fd06c1b2d1
Bappy71 <19103076@iubat.edu>
2023-06-07 17:59:38 +0600

    first commit

--- src/Components/Plans/Plans.js
+++ src/Components/Plans/Plans.js
@@ -6,6 +6,10 @@ import "./Plans.css";
 const Plans = () => {
   return (
     <div className="plans-container">
+
+<div className="blur plans-blur-1"></div>
+<div className="blur plans-blur-2"></div>
+
       <div className="programs-header" style={{ gap: "2rem " }}>
         <span className="strokeText">READY TO START</span>
         <span>YOUR JOURNEY</span>
