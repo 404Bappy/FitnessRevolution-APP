5d3546dc2b2cdfce39d0d31f02c53eaa5a42a76e
Bappy71 <19103076@iubat.edu>
2023-06-08 03:28:19 +0600

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
 import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll/modules";
 
 const Header = () => {
   const mobile = window.innerWidth <= 768 ? true : false;
@@ -10,20 +11,38 @@ const Header = () => {
     <div className="header">
       <img src={Logo} alt="" className="Logo" />
       {menuOpen === false && mobile === true ? (
-        <div style={{ backgroundColor: "var(--appColor)", padding: "0.5rem" }}>
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpen(true)}
+        >
           <img
             src={Bars}
             alt=""
-            style={{ width: "1.5rem", height: "1.5rem", borderRadius: "5px" }}
+            style={{ width: "1.5rem", height: "1.5rem" }}
           />
         </div>
       ) : (
         <ul className="Header-menue">
-          <li>Home</li>
-          <li>Programs</li>
-          <li>Why Us</li>
-          <li>Plans</li>
-          <li>Testimonials</li>
+          <li onClick={() => setMenuOpen(false)}>
+            <Link>Home</Link>
+          </li>
+          <li onClick={() => setMenuOpen(false)}>Programs</li>
+          <li onClick={() => setMenuOpen(false)}>Why Us</li>
+          <li onClick={() => setMenuOpen(false)}>Plans</li>
+          <li>
+            <Link
+              onClick={() => setMenuOpen(false)}
+              to="testimonials"
+              span={true}
+              smooth={true}
+            >
+              Testimonials
+            </Link>
+          </li>
         </ul>
       )}
     </div>
